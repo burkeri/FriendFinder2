1fc303205dfbf2446a177788a2c8a9834acb2145
Richelle Burke <burkeri@alumni.upenn.edu>
2019-01-18 14:36:46 -0500

    final commit -> go to deploy

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,15 +9,65 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res){
 
-        // convert form data into object and add to data
-        var newFriend = req.body;
-        friends.push(newFriend);
-        console.log("server: " + JSON.stringify(newFriend));
+        // create new object from data passed from form
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            answers: []
+        }
 
+        // loop through the answers from the form,
+        // convert them to numbers and
+        // push them to the newFriend object
+        for (i=0; i<=9; i++) {
+            var val = parseFloat(req.body.answers[i]);
+            newFriend.answers.push(val);
+        }
+        
+        // add the new friend to the existing data
+        friends.push(newFriend);
+
+        // *********************************
+
+        // add all the values in the answers array and 
+        // push those sums to a new array
+        var sum = [];
+
+        function addArray (a, b) {
+            return a + b;
+        }
+
+        for (i=0; i < friends.length; i++) {
+            console.log(friends[i].answers);
+            sum.push(friends[i].answers.reduce(addArray)); 
+        }
+
+        console.log(sum);
+
+        // subtract the sum of newewst entry in friends
+        // from the sum of every other entry in friends
+        var diff = [];
+        
+        for (i=0; i < sum.length-1; i++) {
+            diff.push(Math.abs(sum[(sum.length-1)] - sum[i]));
+        }
+
+        console.log(diff);
+
+        // find the smallest number in diff and 
+        // save the index of the smallest number
+        var best = diff.lastIndexOf(Math.min(...diff));
+
+        // the best match is at the index of best
+        console.log(friends[best]);
+        
+        // *********************************
+
+        // best match!
         var data = {
-            name: "p",
-            photo: "p",
-            answers: ["p", "p"]
+            name: friends[best].name,
+            photo: friends[best].photo,
+            answers: friends[best].answers
         }
 
         res.send(data);
